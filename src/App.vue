9fdb3913f49af98cdc22c1667364135c899d0797
zhangwei <wei.zhang@tophant.com>
2023-08-31 14:55:50 +0800

    地图引入

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,26 @@
-<script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
   <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+    <baidu-map
+      class="map"
+      :ak="ak"
+      v="3.0"
+      type="API"
+      :center="{ lng: 116.404, lat: 39.915 }"
+      :zoom="15"
+    ></baidu-map>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
+<script setup lang="ts">
+import { ref } from "vue";
+import { BaiduMap } from "vue-baidu-map-3x";
+
+const ak = ref<string>("mXExT7aleYY4jrDc48VrX2ByYyvap3Hu");
+</script>
+
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+.map {
+  width: 100vw;
+  height: 100vh;
 }
 </style>
