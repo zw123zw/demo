924e1c8ba72e06728cc60fe9a02da680440024f4
zhangwei <wei.zhang@tophant.com>
2023-09-12 20:16:24 +0800

    baidu-map的控件使用

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import { viteExternalsPlugin } from "vite-plugin-externals";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-})
+  plugins: [
+    vue(),
+    viteExternalsPlugin({
+      BMAP_ANCHOR_BOTTOM_LEFT: "BMAP_ANCHOR_BOTTOM_LEFT",
+      BMAP_ANCHOR_BOTTOM_RIGHT: "BMAP_ANCHOR_BOTTOM_RIGHT",
+    }),
+  ],
+});
